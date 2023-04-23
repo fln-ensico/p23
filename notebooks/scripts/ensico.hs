ba19ed8a1386e6554af433281248bb299284cb32
F. Luís Neves <f.luis.neves@ensico.pt>
2023-04-23 17:19:13 +0100

    Updating notebooks...

--- notebooks/scripts/ensico.hs
+++ notebooks/scripts/ensico.hs
@@ -1,5 +1,22 @@
 module ENSICO where
 
+--import Cp
+--import Data.Char
+--import Data.List
+--import Data.List.Split
+
 --- composition ---
 
 (|>) = flip ($)
+
+--- Functional Programming, part II
+
+help x = splitAt (div (length x) 2) x
+
+divide = help
+
+split = help
+
+part1 = fst . divide
+part2 = snd . divide
+
